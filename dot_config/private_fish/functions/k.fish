375436c30d6b8fdf7b4e7a1929285047d48ccd94
Matthew Tattoli <matttattoli@gmail.com>
2021-02-23 17:35:32 -0500

    consume other arguments

--- dot_config/private_fish/functions/k.fish
+++ dot_config/private_fish/functions/k.fish
@@ -1,6 +1,6 @@
 function k --wraps kubectl --description 'default k get events to sort by timestamp'
-  if [ "$argv" = "get events" ];
-    kubectl get events --sort-by=.metadata.creationTimestamp
+  if string match -r -q -- "get events" "$argv"
+    kubectl $argv --sort-by=.metadata.creationTimestamp
   else
     kubectl $argv
   end
